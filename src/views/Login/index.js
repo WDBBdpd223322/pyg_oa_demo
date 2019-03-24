4ae772475796921db0dc4bcf54ec8b2b78e9be8d
wdbbdpd <m13301295501_1@163.com>
2019-03-24 21:07:43 +0800

    login布局
    完成 login 组件的页面布局

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -2,7 +2,16 @@ export default {
   name: 'login',
   data () {
     return {
-      msg: '登陆页面'
+      msg: '登陆页面',
+      loginForm: {},
+      rules: {
+        username: [
+          { required: true, message: '请输入用户邮箱', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
     }
   }
 }
