cea7d2c18b8a4d7cad8fb6e435775fec9e9aa7f4
wdbbdpd <m13301295501_1@163.com>
2019-03-28 15:16:08 +0800

    表单验证
    完成添加用户的表单验证

--- src/components/Custom/UserEdit.vue
+++ src/components/Custom/UserEdit.vue
@@ -1,40 +1,73 @@
 <template>
-  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-    <el-form :model="form">
-      <el-form-item label="活动名称" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+  <el-dialog
+    title="添加用户"
+    :visible.sync="userEditDialog"
+    @close="$refs.userEdit.resetFields()">
+    <el-form
+      class="demo-ruleForm"
+      :model="userEditForm"
+      ref="userEdit"
+      :rules="rules">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="userEditForm.username" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="userEditForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="repeatPass">
+        <el-input v-model="userEditForm.repeatPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="电话" prop="mobile">
+        <el-input v-model="userEditForm.mobile" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="userEditForm.email" autocomplete="off"></el-input>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button @click="dialogFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      <el-button @click="userEditDialog = false">取 消</el-button>
+      <el-button type="primary" @click="addUser">确 定</el-button>
     </div>
   </el-dialog>
 </template>
 
 <script>
+import { checkEmail, checkMobile, checkRepeatPass } from '@/utils/check'
 export default {
   name: 'UserEdit',
   data () {
     return {
-      dialogFormVisible: true,
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      formLabelWidth: '120px'
+      userEditDialog: false,
+      userEditForm: {},
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 2, max: 8, message: '用户名长度为 2 - 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码长度为 6 - 16 个字符', trigger: 'blur' }
+        ],
+        repeatPass: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码长度为 6 - 16 个字符', trigger: 'blur' },
+          { validator: checkRepeatPass.bind(this), trigger: 'blur' }
+        ],
+        mobile: [
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        email: [
+          { validator: checkEmail, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    addUser () {
+      this.$refs.userEdit.validate(valid => {
+        if (!valid) return this.$message.error('请完整填写表单')
+        console.log('准备添加用户')
+      })
     }
   }
 }
--- src/utils/check.js
+++ src/utils/check.js
@@ -1,8 +1,6 @@
 export const checkEmail = (rule, value, callback) => {
-  const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
-  if (!value) {
-    return callback(new Error('邮箱不能为空'))
-  }
+  const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+$/
+  if (!value) return callback()
   setTimeout(() => {
     if (mailReg.test(value)) {
       callback()
@@ -10,4 +8,22 @@ export const checkEmail = (rule, value, callback) => {
       callback(new Error('请输入正确的邮箱格式'))
     }
   }, 100)
+}
+
+export const checkMobile = (rule, value, callback) => {
+  const mailReg = /^\d{11}$/
+  if (!value) return callback()
+  setTimeout(() => {
+    if (mailReg.test(value)) {
+      callback()
+    } else {
+      callback(new Error('请输入正确的手机号码'))
+    }
+  }, 100)
+}
+
+export const checkRepeatPass = function (rule, value, callback) {
+  if (!value) return callback(new Error('请再次输入密码'))
+  if (value !== this.userEditForm.password) return callback(new Error('两次密码不一致'))
+  callback()
 }
